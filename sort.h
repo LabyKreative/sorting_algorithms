9170c5d165afde7d534a2870eef02f03d3c9cbc3
LabyKreative <labykreative@yahoo.com>
2023-04-21 09:48:54 -0700

    Add quick sort prototypes

--- sort.h
+++ sort.h
@@ -39,4 +39,10 @@ void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 
+/* Prototypes - quick sort */
+void swap_ints(int *a, int *b);
+int lomuto_partition(int *array, size_t size, int left, int right);
+void lomuto_sort(int *array, size_t size, int left, int right);
+void quick_sort(int *array, size_t size);
+
 #endif /* SORT_H */
