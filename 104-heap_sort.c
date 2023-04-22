d03c775652edfe741d765d1d0ce358967d046b1b
LabyKreative <labykreative@yahoo.com>
2023-04-22 07:59:18 -0700

    Heap sort

---
+++ 104-heap_sort.c
@@ -0,0 +1,71 @@
+#include "sort.h"
+
+/**
+ * swap_ints - a function that swaps two integers in an array.
+ * @a: first integer to swap.
+ * @b: second integer to swap.
+ */
+void swap_ints(int *a, int *b)
+{
+	int tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+/**
+ * max_heapify - a function that turns a binary tree
+ * into a complete binary heap.
+ * @array: array of integers representing a binary tree.
+ * @size: size of the array/tree.
+ * @base: index of the base row of the tree.
+ * @root: root node of the binary tree.
+ */
+void max_heapify(int *array, size_t size, size_t base, size_t root)
+{
+	size_t left, right, large;
+
+	left = 2 * root + 1;
+	right = 2 * root + 2;
+	large = root;
+
+	if (left < base && array[left] > array[large])
+		large = left;
+	if (right < base && array[right] > array[large])
+		large = right;
+
+	if (large != root)
+	{
+		swap_ints(array + root, array + large);
+		print_array(array, size);
+		max_heapify(array, size, base, large);
+	}
+}
+
+/**
+ * heap_sort - a function that sorts an array of integers in ascending
+ * order using the heap sort algorithm.
+ * @array: array of integers.
+ * @size: size of the array.
+ *
+ * Description: implements the sift-down heap sort
+ * algorithm. prints the array after each swap.
+ */
+void heap_sort(int *array, size_t size)
+{
+	int x;
+
+	if (array == NULL || size < 2)
+		return;
+
+	for (x = (size / 2) - 1; x >= 0; x--)
+		max_heapify(array, size, size, x);
+
+	for (x = size - 1; x > 0; x--)
+	{
+		swap_ints(array, array + x);
+		print_array(array, size);
+		max_heapify(array, size, x, 0);
+	}
+}
